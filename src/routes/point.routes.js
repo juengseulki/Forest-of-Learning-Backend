143ff4d7ceb1f55166eb893226e7c092e17ccb4d
jeungseulki <jsk432726@gmail.com>
2026-04-25 20:04:33 +0900

    refactor(focus/point): 포인트 계산 및 시간 계산 로직 분리 및 구조 개선

--- src/routes/point.routes.js
+++ src/routes/point.routes.js
@@ -5,11 +5,18 @@ import {
   getPointLogs,
 } from '../controllers/point.controller.js';
 import { numericParams } from '../middlewares/validateParams.js';
+import { verifyStudyPasswordByStudyId } from '../middlewares/verifyPassword.js';
 
 const router = express.Router();
 
-router.get('/:studyId', numericParams('studyId'), getPoint);
 router.get('/:studyId/logs', numericParams('studyId'), getPointLogs);
-router.patch('/:studyId', numericParams('studyId'), addPoints);
+router.get('/:studyId', numericParams('studyId'), getPoint);
+
+router.patch(
+  '/:studyId',
+  numericParams('studyId'),
+  verifyStudyPasswordByStudyId,
+  addPoints
+);
 
 export default router;
